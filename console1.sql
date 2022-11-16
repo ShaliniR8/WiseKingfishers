d3f8e13ffe8ed6e1b72bee0784ff95a15d675647
ShaliniR8 <1705820@kiit.ac.in>
2022-11-15 21:28:09 -0600

    Changed sql console1

--- console1.sql
+++ console1.sql
@@ -7,16 +7,37 @@ alter table data_area_beat add column side text;
 update data_area_beat set side='south' where name<1000;
 update data_area_beat set side='north' where name>1000;
 
-select * from data_area_beat;
-select * from trr_trr
-         where id is not null
-           and beat is not null;
+-- convert trr_datetime to year so that we can compute yearly trr count later
+drop table if exists trr_side_temp;
+create temp table trr_side_temp as
+(select
+    beat,
+    extract(year from trr_datetime) as years
+from trr_trr
+where id is not null
+and beat is not null);
+
+-- add side column
+drop table if exists trr_side;
+create temp table trr_side as
+(select beat, years, side from trr_side_temp
+    inner join data_area_beat dab on trr_side_temp.beat = dab.name);
 
 
--- -- trr_trr
--- select id, beat, trr_datetime, officer_assigned_beat, officer_on_duty, officer_in_uniform, officer_rank as rank_during_trr, officer_id from trr_trr
---          where id is not null
---            and beat is not null;
+-- number of trrs by year and side;
+drop table if exists trr_count_by_year_side;
+create temp table trr_count_by_year_side as
+    select beat, side, years, count(*) from trr_side group by (beat, side, years);
+select * from trr_count_by_year_side;
+
+-- number of trrs by year
+drop table if exists trr_count_by_year;
+create temp table trr_count_by_year as
+    select array_agg(distinct beat), years, count(years) from trr_side group by (years);
+select * from trr_count_by_year;
+
+
+
 
 -- relevant officer information
 
